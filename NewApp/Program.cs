c78c11f1e76805ac4073e4c0efcac9d944dce966
dothuthuy <dothuthuy1212002@gmail.com>
2024-03-04 19:51:37 +0700

    toán tư

--- NewApp/Program.cs
+++ NewApp/Program.cs
@@ -1,11 +1,17 @@
-﻿internal class Program
+﻿public class Program
 {
     private static void Main(string[] args)
     {
-        string str = "123";
-        int a = Convert.ToInt32(str);
-        System.Console.WriteLine("a =" + a);
+        int a;
+        System.Console.Write(" a = 5 ");
+        a = Convert.ToInt32(Console.ReadLine());
+        int b;
+        System.Console.Write(" b = 7 ");
+        b = Convert.ToInt32(Console.ReadLine());
+        System.Console.WriteLine("{0} + {1} = {2}",a,b,a+b);
     }
 }
+
+
   
     
