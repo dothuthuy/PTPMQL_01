9350a6e0d79ec7f258a3f1835cbef8e6ff226358
dothuthuy <dothuthuy1212002@gmail.com>
2024-03-05 17:25:23 +0700

    kieu du lieu tra ve cua phuong thuc

--- NewApp/Program.cs
+++ NewApp/Program.cs
@@ -6,12 +6,9 @@ using NEWAPP.Model;
     {
         private static void Main(string[] args)
         {
-            Person ps1 = new Person();
-            Person ps2 = new Person();
-        ps1.FullName = "Do Thu Thuy";
-        ps1.Address =" Nam Dinh";
-        ps1.Age = 18;
-        ps1.Display();
-        ps2.Display();    
+            Person ps = new Person();
+            string str = "Do Thu Thuy";
+            int a = 22;
+            Console.WriteLine("{0} sinh nam {1}", str , ps. GetYearOfBirth(a));
         }
     }
