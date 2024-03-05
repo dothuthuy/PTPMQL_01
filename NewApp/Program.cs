6e32a249e10d6d16453259702a0b3a7c8f96b783
dothuthuy <dothuthuy1212002@gmail.com>
2024-03-05 15:27:49 +0700

    object trong c#

--- NewApp/Program.cs
+++ NewApp/Program.cs
@@ -7,5 +7,9 @@ using NEWAPP.Model;
         {
             Person ps1 = new Person ();
             Person ps2 = new Person ();
+            ps1.FullName = "Nguyen Van A";
+            ps1.Address = " Ha Noi";
+            ps1.Age = 18;
+            ps1.Display();
         }
     }
