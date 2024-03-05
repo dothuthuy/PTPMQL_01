d7b9c5d7da78943aedaaecc36e5fcd6251c8c50c
dothuthuy <dothuthuy1212002@gmail.com>
2024-03-05 15:12:20 +0700

    class lop trog c#

--- NewApp/Model/Employee.cs
+++ NewApp/Model/Employee.cs
@@ -5,21 +5,21 @@ namespace NEWAPP.Model
 public string manhanvien { get; set; }
 public string Ten { get ;set; }
 public int Tuoi { get; set; }
-public int Luong { get; set; }
+public string quequan { get; set; }
 public void EnterData()
 {
     System.Console.Write("manhanvien = ");
-    manhanvien = Console.ReadLine();
+    masinhvien = Console.ReadLine();
     System.Console.Write("Ten = " );
     Ten = Console.ReadLine();
      System.Console.Write("Tuoi = ");
     Tuoi = Convert.ToInt16(Console.ReadLine());
-     System.Console.Write("Luong = ");
-    Luong = Convert.ToInt16(Console.ReadLine());
+     System.Console.Write("quequan = ");
+    quequan = Console.ReadLine();
 }
 public void Display()
     {
-    System.Console.WriteLine("{0} - {1} - {2}tuoi - {3}", manhanvien, Ten, Tuoi, Luong);
+    System.Console.WriteLine("{0} - {1} - {2}tuoi - {3}", masinhvien, Ten, Tuoi, quequan );
 }
     }
 }
