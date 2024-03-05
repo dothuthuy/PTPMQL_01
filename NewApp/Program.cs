db3681531051762e3845fd7c37e63b0b79f9bfdf
dothuthuy <dothuthuy1212002@gmail.com>
2024-03-05 16:19:07 +0700

    object Sinh vien

--- NewApp/Program.cs
+++ NewApp/Program.cs
@@ -6,15 +6,17 @@ using NEWAPP.Model;
     {
         private static void Main(string[] args)
         {
-            Student ps1 = new Student ();
-            Student ps2 = new Student ();
-            ps1.Ten = "Do Thu Thuy";
+            Sinhvien ps1 = new Sinhvien ();
+            Sinhvien ps2 = new Sinhvien ();
+            ps1.Hoten = "Do Thu Thuy";
+            ps1.Diachi = "Nam Dinh";
             ps1.Tuoi = 22;
-            ps1.monhoc = "Hoa hoc";
+            ps1.Sodienthoai = 123456768;
             ps1.Display();
-            ps2.Ten = "Do Huyen Trang";
+            ps2.Hoten = "Do Huyen Trang";
+            ps2.Diachi= "Ha Noi";
             ps2.Tuoi= 21;
-            ps2.monhoc= "Van hoc";
+            ps2.Sodienthoai =44565788;
             ps2.Display();
         }
     }
