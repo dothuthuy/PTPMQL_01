a90b9e7beec4d828c687dc8ffd61b0047e2d3fb6
dothuthuy <dothuthuy1212002@gmail.com>
2024-03-05 15:39:07 +0700

    object trong c#

--- NewApp/Program.cs
+++ NewApp/Program.cs
@@ -11,5 +11,9 @@ using NEWAPP.Model;
             ps1.Address = " Ha Noi";
             ps1.Age = 18;
             ps1.Display();
+            ps2.FullName = "Do Thu Thuy";
+            ps2.Address = "Nam Dinh";
+            ps2.Age = 22;
+            ps2.Display();
         }
     }
