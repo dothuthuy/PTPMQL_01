1fe6628a9c6b27eed872c70dd01b3be603c032f4
dothuthuy <dothuthuy1212002@gmail.com>
2024-03-05 17:19:54 +0700

    class person kieu du lieu tra ve cua phuong thuc

--- NewApp/Model/Person.cs
+++ NewApp/Model/Person.cs
@@ -1,29 +1,11 @@
-namespace NEWAPP.Model
+namespace Your_Namespace
 {
     public class Person
     {
-public string FullName { get; set; }
-
-public string Address { get ;set; }
-public int Age { get ;set; }
-public void EnterData()
+public int GetYearOfBirth(int age)
 {
-    System.Console.Write("Full Name = ");
-    FullName = Console.ReadLine();
-    System.Console.Write("Address = " );
-    Address = Console.ReadLine();
-    System.Console.Write("Age = " );
-    Age = Convert.ToInt16(Console.ReadLine());
+    int yearOfBirth = 2023 - age;
+return yearOfBirth;
 }
-public Person()
-{
-    FullName = "Ho ten mac dinh";
-    Address = "Dia chi mac dinh";
-    Age = 20;
-}
-public void Display2(string ten, int tuoi)
-    {
-    System.Console.WriteLine("{0} -{1} tuoi", ten, tuoi);
-}
- }
+    }
 }
