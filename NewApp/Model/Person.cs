755bf2c7c1674027155af9aa6af5dd4cd1f6152a
dothuthuy <dothuthuy1212002@gmail.com>
2024-03-05 15:36:42 +0700

    class perso

--- NewApp/Model/Person.cs
+++ NewApp/Model/Person.cs
@@ -3,13 +3,17 @@ namespace NEWAPP.Model
     public class Person
     {
 public string FullName { get; set; }
+
 public string Address { get ;set; }
+public int Age { get ;set; }
 public void EnterData()
 {
     System.Console.Write("Full Name = ");
     FullName = Console.ReadLine();
     System.Console.Write("Address = " );
     Address = Console.ReadLine();
+    System.Console.Write("Age = " );
+    Age = Convert.ToInt16(Console.ReadLine());
 }
 public void Display()
     {
