a1ad9bc630951f1560da8a19a2e9a6e8113f33a1
dothuthuy <dothuthuy1212002@gmail.com>
2024-03-05 16:39:33 +0700

    phuong thuc tham so

--- NewApp/Program.cs
+++ NewApp/Program.cs
@@ -6,17 +6,9 @@ using NEWAPP.Model;
     {
         private static void Main(string[] args)
         {
-            Sinhvien ps1 = new Sinhvien ();
-            Sinhvien ps2 = new Sinhvien ();
-            ps1.Hoten = "Do Thu Thuy";
-            ps1.Diachi = "Nam Dinh";
-            ps1.Tuoi = 22;
-            ps1.Sodienthoai = 123456768;
-            ps1.Display();
-            ps2.Hoten = "Do Huyen Trang";
-            ps2.Diachi= "Ha Noi";
-            ps2.Tuoi= 21;
-            ps2.Sodienthoai =44565788;
-            ps2.Display();
+            Person ps = new Person();
+        string str = "Do Thu Thuy";
+        int a = 22;
+        ps.Display2(str, a);      
         }
     }
