38fb0071efe2401d0cbc6d38ee707c763125989a
dothuthuy <dothuthuy1212002@gmail.com>
2024-03-05 23:43:35 +0700

    object emmployee ke thua person

--- NewApp/Program.cs
+++ NewApp/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.Design.Serialization;
+﻿using System.Collections.Specialized;
+using System.ComponentModel.Design.Serialization;
 using System.Reflection;
 using NEWAPP.Model;
 
@@ -6,11 +7,9 @@ using NEWAPP.Model;
     {
         private static void Main(string[] args)
         {
-            Student ps = new Student();
-            string str = "Do Thu Thuy";
-            int b = 25;
-            string str = "hoa hoc";
-            Console.WriteLine("{0} - {1} - {2}",str , ps.GetYearOfBirth(b) ,str );
+            Employee std = new Employee();
+        std.EnterData();
+        std.Display();
         }
-        }
+    }
     
