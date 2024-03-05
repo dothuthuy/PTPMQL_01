742ed3e42d82165fc3f2cd8d9a6a72a4b8ac2ecf
dothuthuy <dothuthuy1212002@gmail.com>
2024-03-05 21:36:01 +0700

    class employee kieu du lieu tra ve

--- NewApp/Model/Employee.cs
+++ NewApp/Model/Employee.cs
@@ -6,9 +6,10 @@ public int manhanvien { get; set; }
 public string Ten { get ;set; }
 public int Tuoi { get; set; }
 public int Luong { get; set; }
-public void Display2( int manhanvien, string Ten, int Tuoi, int Luong)
+public int GetYearOfBirth(int Tuoi)
 {
-    System.Console.WriteLine("{0} - {1} - {2} tuoi - {3}", manhanvien , Ten, Tuoi, Luong);
-    }
+    int yearOfBirth = 2023 - Tuoi;
+    return yearOfBirth;
+}
     }
 }
