b0e0964d32f4378db868b456fd2851e07c7a961c
dothuthuy <dothuthuy1212002@gmail.com>
2024-03-06 10:17:15 +0700

    class employee xu ly ngoại le

--- NewApp/Model/Employee.cs
+++ NewApp/Model/Employee.cs
@@ -4,20 +4,36 @@ namespace NEWAPP.Model
 {
     public class Employee
     {
-public class Employee : Person
-{
-    public string EmployeeCode { get; set; }
-}
+public int manhanvien { get; set;}
+public string Ten { get; set;}
+public int Tuoi { get; set;}
+public int Luong { get; set;}
 public void EnterData()
 {
-    base.EnterData();
-    System.Console.Write("Employee Code = ");
-    EmployeeCode = Console.ReadLine();
-}
-public void Display()
-{
-    base.Display();
-    System.Console.Write("- manhanvien: {0}", EmployeeCode);
+    System.Console.Write("manhanvien =");
+    try{
+        manhanvien = Convert.ToInt16(Console.ReadLine());
+    }catch(Exception e)
+    {
+        manhanvien = 0;
+    }
+    
+    System.Console.Write("Ten =");
+    Ten = Console.Readline();
+    System.Console.Write("Tuoi =");
+    try{
+        Tuoi = Convert.ToInt16(Console.ReadLine());
+    }catch(Exception e)
+    {
+        Tuoi = 0;
+    }
+    System.Console.Write("Luong =");
+    try{
+        Luong = Convert.ToInt16(Console.ReadLine());
+    }catch(Exception e)
+    {
+        Luong = 0;
+    }
 }
     }
 }
