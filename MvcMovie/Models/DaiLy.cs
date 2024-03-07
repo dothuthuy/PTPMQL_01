82589d62b60ef838375effe0a95921d7ac781c4b
dothuthuy <dothuthuy1212002@gmail.com>
2024-03-07 18:22:15 +0700

    tao class Daily trong model

---
+++ MvcMovie/Models/DaiLy.cs
@@ -0,0 +1,12 @@
+using System.Data;
+
+namespace MvcMovie.Models;
+
+public  class DaiLy:HeThongPhanPhoi
+{
+    public string? MaDaiLy {get; set;}
+    public string? DiaChi {get; set;}
+    public string? NguoiDaiDien {get; set;}
+    public string? DienThoai {get; set;}
+
+    }
