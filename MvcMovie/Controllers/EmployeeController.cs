58c63111b933cd3cf4afeffcd4a63ea9f057419d
dothuthuy <dothuthuy1212002@gmail.com>
2024-03-08 14:55:55 +0700

    tao view index cua employeeControler

--- MvcMovie/Controllers/EmployeeController.cs
+++ MvcMovie/Controllers/EmployeeController.cs
@@ -4,19 +4,11 @@ namespace MvcMovie.Controllers
 {
     public class EmployeeController : Controller
     { 
-        // GET: /Employee/
-    
+
         public IActionResult Index()
         {
             return View();
         }
         
-        
-        // GET: /Employee/Welcome/ 
-
-        public string Welcome()
-        {
-            return "Xin chao ...";
-        }
     }
 }
