5ec90117235d887f2d6a40947ec9670addcad013
dothuthuy <dothuthuy1212002@gmail.com>
2024-03-09 19:07:32 +0700

    employee controller tra yeu cau da xu ly cho view

--- MvcMovie/Controllers/EmployeeController.cs
+++ MvcMovie/Controllers/EmployeeController.cs
@@ -1,4 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
+using MvcMovie.Models;
+using System.Reflection.Metadata.Ecma335;
 using System.Text.Encodings.Web;
 namespace MvcMovie.Controllers
 {
@@ -9,6 +11,13 @@ namespace MvcMovie.Controllers
         {
             return View();
         }
-        
+        [HttpPost]
+    public IActionResult Index ( Empolyee ps)
+    { 
+        string strOutput = "xin chao" + ps.PersonId + ps.FullName + ps.Address + ps.EmployeeId + ps.Age;
+        ViewBag.info=strOutput;
+        return View();
+    }
+        }
     }
 }
