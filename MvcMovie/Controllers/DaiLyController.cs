6edd1aca6ca5f919f3e486892809d48a6856f81c
dothuthuy <dothuthuy1212002@gmail.com>
2024-03-11 22:07:02 +0700

    trả lại yêu cầu đã được xử lý daily controller lên view controller

--- MvcMovie/Controllers/DaiLyController.cs
+++ MvcMovie/Controllers/DaiLyController.cs
@@ -1,19 +1,22 @@
-using System.Reflection.Metadata.Ecma335;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using MvcMovie.Models;
 
 namespace MvcMovie.Controllers
 {
-    public class DaiLy : Controller
+    public class DaiLyController : Controller
     {
         public IActionResult Index()
         {
             return View();
         }
+        [HttpPost]
         public IActionResult Index(string MaDaiLy , string TenDaiLy , string DiaChi , string NguoiDaiDien , string DienThoai , string MaHTPP, string TenHTPP)
         {
             string strOutput = "Xin chao" +  MaDaiLy + TenDaiLy + DiaChi + NguoiDaiDien + DienThoai +  MaHTPP + TenHTPP;
             ViewBag.inforDaiLy=strOutput;
             return View();
-        }
-        }
+        } 
     }
+}
--- MvcMovie/Models/DaiLy.cs
+++ MvcMovie/Models/DaiLy.cs
@@ -1,12 +1,13 @@
 using System.Data;
 
-namespace MvcMovie.Models;
-
-public  class DaiLy:HeThongPhanPhoi
+namespace MvcMovie.Models
 {
-    public string? MaDaiLy {get; set;}
-    public string? DiaChi {get; set;}
-    public string? NguoiDaiDien {get; set;}
-    public string? DienThoai {get; set;}
+    public  class DaiLy: HeThongPhanPhoi
+    {
+        public string? MaDaiLy {get; set;}
+        public string? DiaChi {get; set;}
+        public string? NguoiDaiDien {get; set;}
+        public string? DienThoai {get; set;}
 
-    }
+        }
+}
