51d7f58bff380b47a90a2d8e36ef6315f18ddc2d
dothuthuy <dothuthuy1212002@gmail.com>
2024-03-11 20:08:14 +0700

    gui len view khi da xu ly yeu cau he thong

--- MvcMovie/Controllers/HeThongPhanPhoiController.cs
+++ MvcMovie/Controllers/HeThongPhanPhoiController.cs
@@ -1,12 +1,25 @@
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using MvcMovie.Models;
+
 
 namespace MvcMovie.Controllers
 {
     public class HeThongPhanPhoi : Controller
     {
+
         public IActionResult Index()
         {
             return View();
+        }
+        [HttpPost]
+        public IActionResult Index(string MaHTPP , string TenHTPP)
+        {
+            string strOutput = "Xin chao" + "," + MaHTPP +  TenHTPP;
+            ViewBag.infoHeThongPhanPhoi=strOutput;
+            return View();
+
         }
 
     }
