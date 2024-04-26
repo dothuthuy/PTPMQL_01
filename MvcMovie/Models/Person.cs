4685443844f6177776bd247e02219d5428a6f5f9
dothuthuy <dothuthuy1212002@gmail.com>
2024-04-26 08:42:31 +0700

    Chỉnh sửa personController

--- MvcMovie/Models/Person.cs
+++ MvcMovie/Models/Person.cs
@@ -1,11 +1,18 @@
-using System.Data;
 
-namespace MvcMovie.Models;
+
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MvcMovie.Models
+{
+     [Table("Persons")]
 
 public  class Person
 {
+     [Key]
      public string? PersonId {get; set;}
     public string? FullName {get; set;}
     public string? Address {get;set;}
 
     }
+}
--- MvcMovie/Program.cs
+++ MvcMovie/Program.cs
@@ -1,6 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+using MvcMovie.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.")));
+
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
