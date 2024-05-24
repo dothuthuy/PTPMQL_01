6a489ebb0a1a9f0445a511848253eb33755ebfae
dothuthuy <dothuthuy1212002@gmail.com>
2024-05-24 17:29:20 +0700

    Phan trang

--- MvcMovie/Controllers/PersonController.cs
+++ MvcMovie/Controllers/PersonController.cs
@@ -9,6 +9,7 @@ using Microsoft.EntityFrameworkCore;
 using MvcMovie.Data;
 using MvcMovie.Models;
 using MvcMovie.Models.Process;
+using X.PagedList;
 
 namespace MvcMovie.Controllers
 {
@@ -23,10 +24,24 @@ namespace MvcMovie.Controllers
         }
 
         // GET: Person
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index(int? page, int? PageSize)
+        
         {
-            return View(await _context.Person.ToListAsync());
+            ViewBag.PageSize = new List<SelectListItem>()
+            {
+            new SelectListItem() { Value="3", Text="3"},
+            new SelectListItem() { Value="5", Text="5"},
+            new SelectListItem() { Value="10", Text="10"},
+            new SelectListItem() { Value="15", Text="15"},
+            new SelectListItem() { Value="25", Text="25"},
+            new SelectListItem() { Value="50", Text="50"},
+        }; 
+        int pagesize =(PageSize ?? 3);
+        ViewBag.Psize = pagesize;
+            var model = _context.Person.ToList().ToPagedList(page ?? 1, pagesize);
+            return View(model);
         }
+
        [HttpPost]
         public async Task<IActionResult> Index (string SearchString)
         {
